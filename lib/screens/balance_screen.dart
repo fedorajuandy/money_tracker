a92c99fe5ed74e0164c443564ef90832948bc39f
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-11-18 03:44:51 +0700

    balance f-ed up

--- lib/screens/balance_screen.dart
+++ lib/screens/balance_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:money_tracker/themes/colors.dart';
+import 'package:money_tracker/themes/spaces.dart';
 
 class BalanceScreen extends StatefulWidget {
   const BalanceScreen({super.key});
@@ -10,6 +12,69 @@ class BalanceScreen extends StatefulWidget {
 class _BalanceScreenState extends State<BalanceScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: dark.withOpacity(0.05),
+      body: screen(),
+    );
+  }
+
+  Widget screen() {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: primary,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: secondary.withOpacity(0.01),
+            spreadRadius: 10,
+            blurRadius: 3,
+          ),
+        ]
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 25, bottom: 25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  "Total money",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                    color: Colors.white
+                  ),
+                ),
+                sbh10(),
+                const Text(
+                  "Rp100,000.00",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.white
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.white),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  "Edit",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
