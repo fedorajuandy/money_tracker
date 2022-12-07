3e4b792796abd6d028bfb6d4732e52b5dfa7e552
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-07 19:07:21 +0700

    getters and setters added

--- lib/models/report.dart
+++ lib/models/report.dart
@@ -2,11 +2,12 @@ class Report {
   double dailySum = 0;
   double monthlyExpense = 0;
   double monthlyIncome = 0;
-  double yearlyIncome = 0;
   double yearlyExpense = 0;
-  double lowestIncome = 0;
+  double yearlyIncome = 0;
   double lowestExpense = 0;
-  double highgestExpense = 0;
+  double highestExpense = 0;
+  double lowestIncome = 0;
+  double highestIncome = 0;
 
   resetDailySum() {
     dailySum = 0;
@@ -26,5 +27,75 @@ class Report {
   }
   double getMonthlyExpense() {
     return monthlyExpense;
+  }
+
+  resetMonthlyIncome() {
+    monthlyIncome = 0;
+  }
+  addMonthlyIncome(double a) {
+    monthlyIncome += a;
+  }
+  double getMonthlyIncome() {
+    return monthlyIncome;
+  }
+
+  resetYearlyExpense() {
+    yearlyExpense = 0;
+  }
+  addYearlyExpense(double a) {
+    yearlyExpense += a;
+  }
+  double getYearlyExpense() {
+    return yearlyExpense;
+  }
+
+  resetYearlyIncome() {
+    yearlyIncome = 0;
+  }
+  addYearlyIncome(double a) {
+    yearlyIncome += a;
+  }
+  double getYearlyIncome() {
+    return yearlyIncome;
+  }
+
+  resetLowestExpense() {
+    lowestExpense = 0;
+  }
+  addLowestExpense(double a) {
+    lowestExpense += a;
+  }
+  double getLowestExpense() {
+    return lowestExpense;
+  }
+
+  resetHighestExpense() {
+    highestExpense = 0;
+  }
+  addHighestExpense(double a) {
+    highestExpense += a;
+  }
+  double getHighestExpense() {
+    return highestExpense;
+  }
+
+  resetLowestIncome() {
+    lowestIncome = 0;
+  }
+  addLowestIncome(double a) {
+    lowestIncome += a;
+  }
+  double getLowestIncome() {
+    return lowestIncome;
+  }
+
+  resetHighestIncome() {
+    highestIncome = 0;
+  }
+  addHighestIncome(double a) {
+    highestIncome += a;
+  }
+  double getHighestIncome() {
+    return highestIncome;
   }
 }
