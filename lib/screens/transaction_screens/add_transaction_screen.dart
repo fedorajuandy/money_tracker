682c024047d5aa53637a0508976297e8d475dc0b
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-11 03:17:24 +0700

    tidied up

--- lib/screens/transaction_screens/add_transaction_screen.dart
+++ lib/screens/transaction_screens/add_transaction_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:money_tracker/operations/transaction_operation.dart';
-import 'package:money_tracker/models/new_transaction.dart';
 import 'package:money_tracker/themes/colors.dart';
 import 'package:money_tracker/themes/spaces.dart';
 import 'package:money_tracker/themes/text_formats.dart';
+import 'package:money_tracker/models/balance.dart';
+import 'package:money_tracker/models/new_transaction.dart';
+import 'package:money_tracker/operations/balance_operation.dart';
+import 'package:money_tracker/operations/transaction_operation.dart';
+import 'package:money_tracker/widgets/button.dart';
+import 'package:money_tracker/widgets/textfield.dart';
 import 'package:money_tracker/widgets/title.dart';
 
 class AddTransactionScreen extends StatefulWidget {
@@ -21,9 +25,9 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
   final TextEditingController _amountText = TextEditingController();
   final TextEditingController _dateText = TextEditingController();
   final TextEditingController _timeText = TextEditingController();
-  final transactionOperation = TransactionOperation();
-  late DatabaseReference dbTransaction = FirebaseDatabase.instance.ref().child('transactions');
   late DatabaseReference dbBalance;
+  final transactionOperation = TransactionOperation();
+  final balanceOperation = BalanceOperation();
   DateTime now = DateTime.now();
   int activeType = 0;
   double _amount = 0;
@@ -53,7 +57,7 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
   Widget screen() {
     return Column(
       children: <Widget>[
-        // the 'header'
+        // header
         Container(
           decoration: BoxDecoration(
             color: dark.withOpacity(0.05),
@@ -74,6 +78,7 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
             ),
           ),
         ),
+        // content
         Expanded(
           child: SingleChildScrollView(
             physics: const ScrollPhysics(),
@@ -100,11 +105,11 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
                     key: _keyform,
                     child: Column(
                       children: <Widget>[
-                        textFieldName(),
+                        textField("Transaction name", _nameText, "Please input transaction name"),
                         sbh20(),
-                        textFieldCategory(),
+                        textField("Category", _categoryText, "Please input transaction category"),
                         sbh20(),
-                        textFieldAmount(),
+                        textFieldNumber("Amount" ,_amountText, "Please input transaction amount"),
                         sbh20(),
                         pickDate(),
                         sbh20(),
@@ -113,7 +118,7 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            buttonCancel(),
+                            buttonCancel(context),
                             sbw8(),
                             buttonAdd(),
                           ],
@@ -160,7 +165,7 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
             color: white,
             border: Border.all(
               width: 2,
-              color: activeType == 0 ? primary : Colors.transparent
+              color: activeType == 0 ? primary : Colors.transparent,
             ),
             borderRadius: BorderRadius.circular(12),
             boxShadow: [
@@ -217,7 +222,7 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
             color: white,
             border: Border.all(
               width: 2,
-              color: activeType == 1 ? red : Colors.transparent
+              color: activeType == 1 ? red : Colors.transparent,
             ),
             borderRadius: BorderRadius.circular(12),
             boxShadow: [
@@ -257,93 +262,11 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
     );
   }
 
-  Widget textFieldName() {
-    return TextFormField(
-      cursorColor: primary,
-      style: const TextStyle(color: dark),
-      decoration: const InputDecoration(
-        hintText: "Transaction name",
-        hintStyle: TextStyle(
-          color: Colors.black54,
-        ),
-        labelText: 'Transaction name',
-        labelStyle: TextStyle(
-          color: primary,
-        ),
-        border: OutlineInputBorder(),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: primary,
-          ),
-        ),
-      ),
-      controller: _nameText,
-      validator: (value) {
-        return (value !.isEmpty? "Please input transaction name": null);
-      },
-    );
-  }
-
-  Widget textFieldCategory() {
-    return TextFormField(
-      cursorColor: primary,
-      style: const TextStyle(color: dark),
-      decoration: const InputDecoration(
-        hintText: "Category",
-        hintStyle: TextStyle(
-          color: Colors.black54,
-        ),
-        labelText: "Category",
-        labelStyle: TextStyle(
-          color: primary,
-        ),
-        border: OutlineInputBorder(),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: primary,
-          ),
-        ),
-      ),
-      controller: _categoryText,
-      validator: (value) {
-        return (value !.isEmpty? "Please input transaction category": null);
-      },
-    );
-  }
-
-  Widget textFieldAmount() {
-    return TextFormField(
-      cursorColor: primary,
-      style: const TextStyle(color: dark),
-      decoration: const InputDecoration(
-        hintText: "Amount",
-        hintStyle: TextStyle(
-          color: Colors.black54,
-        ),
-        labelText: 'Amount',
-        labelStyle: TextStyle(
-          color: primary,
-        ),
-        border: OutlineInputBorder(),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: primary,
-          ),
-        ),
-      ),
-      keyboardType: TextInputType.number,
-      controller: _amountText,
-      validator: (value) {
-        return (value !.isEmpty? "Please input transaction amount": null);
-      },
-    );
-  }
-
   Widget pickDate() {
     return TextFormField(
       controller: _dateText,
       validator: (value) {
-        return (value !.isEmpty? "Please input transaction date": null);
+        return (value !.isEmpty? "Please input transaction date" : null);
       },
       readOnly: true,
       style: const TextStyle(color: dark),
@@ -351,7 +274,7 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
         hintStyle: TextStyle(
           color: Colors.black54,
         ),
-        labelText: 'Date',
+        labelText: "Date",
         labelStyle: TextStyle(
           color: primary,
         ),
@@ -399,7 +322,7 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
     return TextFormField(
       controller: _timeText,
       validator: (value) {
-        return (value !.isEmpty? "Please input transaction time": null);
+        return (value !.isEmpty? "Please input transaction time" : null);
       },
       readOnly: true,
       style: const TextStyle(color: dark),
@@ -407,7 +330,7 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
         hintStyle: TextStyle(
           color: Colors.black54,
         ),
-        labelText: 'Time',
+        labelText: "Time",
         labelStyle: TextStyle(
           color: primary,
         ),
@@ -463,30 +386,16 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
         if (_keyform.currentState !.validate()) {
           final newTransaction = NewTransaction(activeType, _nameText.text, _categoryText.text, double.parse(_amountText.text), _dateText.text, _timeText.text);
           transactionOperation.add(newTransaction);
+
           if(activeType == 0) {
             _amount += double.parse(_amountText.text);
           } else {
             _amount -= double.parse(_amountText.text);
           }
+          final newBalance = Balance(_amount);
+          balanceOperation.update(newBalance);
 
-          Map<String, dynamic> balance = {
-            'amount': _amount,
-          };
-
-          /* Map<String, dynamic> transaction = {
-            'type': activeType,
-            'name': _nameText.text,
-            'category': _categoryText.text,
-            'amount': _amountText.text,
-            'date': _dateText,
-            'time': _timeText,
-          };
-
-          dbTransaction.push().set(transaction); */
-
-          dbBalance.child("user0").update(balance).then((value) => {
-            Navigator.pop(context),
-          });
+          Navigator.pop(context);
         } else {
           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
             content: Text("Please enter all required fields."),
@@ -495,26 +404,6 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
         }
       },
       child: const Text("Add transaction"),
-    );
-  }
-
-  Widget buttonCancel() {
-    return TextButton(
-      style: TextButton.styleFrom(
-        padding: const EdgeInsets.all(20),
-        backgroundColor: white,
-        foregroundColor: primary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-          side: const BorderSide(
-            color: primary,
-          ),
-        ),
-      ),
-      onPressed: () {
-        Navigator.pop(context);
-      },
-      child: const Text("Cancel"),
     );
   }
 }
